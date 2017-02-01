18cf0056dfafb1732a2e0ba163e97a6a425d843f
juuliferreiro <juuliferreiro18@gmail.com>
2017-02-01 11:58:03 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,16 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+
+	var nombre;//definir variable 
+
+	nombre="Julieta";//cargo variable 
+
+	nombre=prompt("ingrese su nombre","natalia natalia");
+	alert("nombre : "+nombre);
+	//esto es un comentario de doble linea 
+	/*hsbdhdhshsdhdsdh*/
+	var person = prompt("julieta ferreiro", "natalia natalia");
 }
 
+ 
