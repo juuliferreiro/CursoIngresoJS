776b4be98c7dc57e57ff1051f4afed7a75c26a5b
juuliferreiro <juuliferreiro18@gmail.com>
2017-02-07 12:07:00 -0300

    Add files via upload

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,12 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+	var nombre;
+	nomre=prompt("ingresar nombre");
+	document.getElementById("elNombre").value=nombre;
+	
+}
+
---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,21 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+Mostar el resulto por medio de "ALERT"*/
+function sumar()
+{
+var numero1;
+var numero2;
+var resultado;
+
+
+    numero1=document.getElementById("numeroUno").value;
+     numero2=document.getElementById("numeroDos").value;
+
+numero1=parseInt(numero1);
+numero2=parseInt(numero2);
+resultado=nuemero1+numero2;
+alert("el resultado"+resultado);
+  
+
+}
+
